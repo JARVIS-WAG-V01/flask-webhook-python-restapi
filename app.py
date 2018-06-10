a7110b4333c592aa9eaef9ea19b77a249fdc991b
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-06-10 16:36:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ user= "000d5b69-974a-4f7b-9118-5bcb7aed2484-bluemix"
 password= "ecab228d0b549390a55b708bcfe28f6818e223aa5761dc2628e65859c7211e87"	
 host= "000d5b69-974a-4f7b-9118-5bcb7aed2484-bluemix.cloudant.com"
 url = 'https://' + host
-client = Cloudant9user, password, url=url, connect=True)
+client = Cloudant(user, password, url=url, connect=True)
 app=Flask(__name__)
 
 @app.route('/webhook',methods=['POST'])
