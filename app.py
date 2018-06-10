7bc6fb936d40fffb233e73357aae8f73cd158cd5
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-06-10 14:48:26 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+import os
+from flask import Flask, request, make_response, render_template, session, g
+import time
+
+user= "000d5b69-974a-4f7b-9118-5bcb7aed2484-bluemix"	
+password= "ecab228d0b549390a55b708bcfe28f6818e223aa5761dc2628e65859c7211e87"	
+host= "000d5b69-974a-4f7b-9118-5bcb7aed2484-bluemix.cloudant.com"
+
+app=Flask(_name__)
+
+@app.route('/webhook',methods=['POST'])
+def webhook():
+    return "Webhook call success"
+
+port = os.getenv('VCAP_APP_PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port), use_reloader=True, debug=True)
