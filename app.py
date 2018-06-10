d730af7c9786cda829b98827bb32624b657c33e7
Gunalan S <34304620+s-gunalan@users.noreply.github.com>
2018-06-10 14:50:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ user= "000d5b69-974a-4f7b-9118-5bcb7aed2484-bluemix"
 password= "ecab228d0b549390a55b708bcfe28f6818e223aa5761dc2628e65859c7211e87"	
 host= "000d5b69-974a-4f7b-9118-5bcb7aed2484-bluemix.cloudant.com"
 
-app=Flask(_name__)
+app=Flask(__name__)
 
 @app.route('/webhook',methods=['POST'])
 def webhook():
